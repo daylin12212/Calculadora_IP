dac58e20f16b715be2a4a8b92976f5671978e279
juanjferote <juanj.ferote@educa.jcyl.es>
2025-05-13 19:40:07 +0200

    funcionando

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const octetos = document.querySelectorAll('.octeto');
 
-// Obtener el formulario y el resultado
 octetos.forEach(octeto => {
     octeto.addEventListener('input', () => {
         if (octeto.value <= 255 && octeto.value >= 0) {
@@ -11,38 +10,36 @@ octetos.forEach(octeto => {
     });
 });
 
-//funcion para almacenar los datos
-function almacenarDatos() {
-    let primer_octeto = document.getElementById("primer_octeto").value
-    let segundo_octeto = document.getElementById("segundo_octeto").value
-    let tercer_octeto = document.getElementById("tercer_octeto").value
-    let cuarto_octeto = document.getElementById("cuarto_octeto").value
-    let ip_completa = document("ip_completa").value = primer_octeto + "." + segundo_octeto + "." + tercer_octeto + "." + cuarto_octeto
-    let mascara = document.getElementById("ip_mascara").value;
-    //const ip_completa = document.getElementById("ip_completa").innerText = primer_octeto + "." + segundo_octeto + "." + tercer_octeto + "." + cuarto_octeto
-}
 
-function ipClase() {
-    almacenarDatos()
-    if (primer_octeto >= 0 && primer_octeto <= 127){
-        document.getElementById("ip_clase").innerText = "Clase A"
-    }
-    if (primer_octeto >= 128 && primer_octeto <= 191){
-        document.getElementById("ip_clase").innerText = "Clase A"
-    }
-    if (primer_octeto >= 192 && primer_octeto <= 223){
-        document.getElementById("ip_clase").innerText = "Clase A"
-    }
-    if (primer_octeto >= 0 && primer_octeto <= 127){
-        document.getElementById("ip_clase").innerText = "Clase A"
+function ipMascaraClase(primer_octeto) {
+    if (primer_octeto >= 0 && primer_octeto <= 127) {
+        document.getElementById("ip_clase").innerText = "Clase A";
+        document.getElementById("ip_mascara").innerText = "225.0.0.0";
+    } if (primer_octeto >= 128 && primer_octeto <= 191) {
+        document.getElementById("ip_clase").innerText = "Clase B";
+        document.getElementById("ip_mascara").innerText = "225.225.0.0";
+    } if (primer_octeto >= 192 && primer_octeto <= 223) {
+        document.getElementById("ip_clase").innerText = "Clase C";
+        document.getElementById("ip_mascara").innerText = "225.225.225.0";
+    } if (primer_octeto >= 224 && primer_octeto <= 239) {
+        document.getElementById("ip_clase").innerText = "Clase D";
+        document.getElementById("ip_mascara").innerText = "No tiene mascara";
+    } if (primer_octeto >= 240 && primer_octeto <= 255) {
+        document.getElementById("ip_clase").innerText = "Clase E";
+        document.getElementById("ip_mascara").innerText = "No tiene mascara";
     }
 }
 
 // Obtener los elementos del formulario y el resultado
 function mostrarResultado() {
+    const primer_octeto = document.getElementById("primer_octeto").value
+    const segundo_octeto = document.getElementById("segundo_octeto").value
+    const tercer_octeto = document.getElementById("tercer_octeto").value
+    const cuarto_octeto = document.getElementById("cuarto_octeto").value
     formulario.style.display = "none"
     resultado.style.display = "block"
-    almacenarDatos();
+    ipMascaraClase(primer_octeto);
+    document.getElementById("ip_completa").innerText = primer_octeto + "." + segundo_octeto + "." + tercer_octeto + "." + cuarto_octeto;
 
 }
 
@@ -57,23 +54,6 @@ document.getElementById("formulario").addEventListener('submit', function(event)
             alert("¡Hay un campo con valor inválido!");
             break; 
         }
-    }
-
-    if (primer_octeto >= 1 && primer_octeto <= 126) {
-        document.getElementById("ip_clase").innerHTML = "Clase A";
-        ip_mascara = "225.000.000.000";
-    } else if (primer_octeto >= 128 && primer_octeto <= 191) {
-        document.getElementById("ip_clase").innerHTML = "Clase B";
-        ip_mascara = "225.225.000.000";
-    } else if (primer_octeto >= 192 && primer_octeto <= 223) {
-        document.getElementById("ip_clase").innerHTML = "Clase C";
-        ip_mascara = "225.225.225.000";
-    } else if (primer_octeto >= 224 && primer_octeto <= 239) {
-        document.getElementById("ip_clase").innerHTML = "Clase D";
-        ip_mascara = "no tiene mascara";
-    } else if (primer_octeto >= 240 && primer_octeto <= 255) {
-        document.getElementById("ip_clase").innerHTML = "Clase E";
-        ip_mascara = "no tiene mascara";
     }
 
     if (!octetoNoValido) {
