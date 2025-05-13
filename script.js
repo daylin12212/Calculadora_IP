e503610b2f92c021ac3900f852f77283647f57e4
juanjferote <juanj.ferote@educa.jcyl.es>
2025-05-13 16:55:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const octetos = document.querySelectorAll('.octeto');
 
-
-// Evento para ver si es válido el valor de cada octeto
-// y cambiar el color de fondo a verde o rojo
 octetos.forEach(octeto => {
     octeto.addEventListener('input', () => {
         const valor = parseInt(octeto.value);
 
-        // Verificar si el valor está en el rango permitido
-        if (!isNaN(valor) && valor >= 0 && valor <= 255) {
+        if (valor <= 255 && valor >= 0) {
             octeto.style.backgroundColor = "green"; // Dentro del rango
         } else {
             octeto.style.backgroundColor = "red"; // Fuera del rango
