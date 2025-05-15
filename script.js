504795a1cc7870472e993fd2948e9ceb17873eac
juanjferote <juanj.ferote@educa.jcyl.es>
2025-05-15 19:35:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,43 +47,47 @@ document.getElementById("menu_principal").addEventListener('click', () => {
 });
 
 function caracteristicasIP(primer_octeto, segundo_octeto, tercer_octeto, clase, mascara, wildcard, direccion_red, direccion_broadcast, hosts) {
-let mascara_personalizada = document.getElementById("mascara_personalizada");
+    let mascara_personalizada = document.getElementById("mascara_personalizada");
+
 
     let bistHosts = 32 - mascara_personalizada.value;
+    let numero_subredes;
+
+    let bits_predeterminados;
 
     if (primer_octeto >= 0 && primer_octeto <= 127) {
         clase = "Clase A";
+        bits_predeterminados = 8; 
         mascara = "255.0.0.0";
         wildcard = "0.255.255.255";
         direccion_red = primer_octeto + ".0.0.0";
         direccion_broadcast = primer_octeto + ".255.255.255";
-        hosts = Math.pow(2, bistHosts) - 2;
-        let bist_prestadosA = mascara_personalizada.value - 8;
+
+        let bist_prestadosA = mascara_personalizada.value - bits_predeterminados;
         numero_subredes = Math.pow(2, bist_prestadosA);
-
-
+        
     } else if (primer_octeto >= 128 && primer_octeto <= 191) {
         clase = "Clase B";
+        bits_predeterminados = 16; 
         mascara = "255.255.0.0";
         wildcard = "0.0.255.255";
         direccion_red = primer_octeto + "." + segundo_octeto + ".0.0";
         direccion_broadcast = primer_octeto + "." + segundo_octeto + ".255.255";
-        hosts = Math.pow(2, bistHosts) - 2;
-        let bist_prestadosB = mascara_personalizada.value - 8;
+        
+        let bist_prestadosB = mascara_personalizada.value - bits_predeterminados;
         numero_subredes = Math.pow(2, bist_prestadosB);
-
-
+        
     } else if (primer_octeto >= 192 && primer_octeto <= 223) {
         clase = "Clase C";
+        bits_predeterminados = 24; 
         mascara = "255.255.255.0";
         wildcard = "0.0.0.255";
         direccion_red = primer_octeto + "." + segundo_octeto + "." + tercer_octeto + ".0";
         direccion_broadcast = primer_octeto + "." + segundo_octeto + "." + tercer_octeto + ".255";
-        hosts = Math.pow(2,bistHosts) - 2;
-        let bist_prestadosC = mascara_personalizada.value - 8;
+        
+        let bist_prestadosC = mascara_personalizada.value - bits_predeterminados;
         numero_subredes = Math.pow(2, bist_prestadosC);
-
-
+        
     } else if (primer_octeto >= 224 && primer_octeto <= 239) {
         clase = "Clase D";
         mascara = "No tiene máscara";
@@ -102,6 +106,8 @@ let mascara_personalizada = document.getElementById("mascara_personalizada");
         hosts = "No tiene número de hosts";
         numero_subredes = "No tiene número subredes";
     }
+
+    hosts = Math.pow(2, bistHosts) - 2;
 
     document.getElementById("clase_red").innerText = clase;
     document.getElementById("mascara_subred").innerText = mascara;
@@ -134,7 +140,6 @@ function mostrarResultado(ip) {
     caracteristicasIP(primer_octeto, segundo_octeto, tercer_octeto);
     tipoRed(primer_octeto);
 
-    // Mostrar IP normal (sin colores)
     document.getElementById("ip_completa").innerText = ip;
     document.getElementById("ip_completa").style.display = "";
     document.getElementById("ip_binario").style.display = "none";
